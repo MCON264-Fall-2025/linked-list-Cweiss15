1d9182268c85f2174d6fab28f2663fab71b4af43
Cweiss15 <cweiss15@student.touro.edu>
2025-10-22 15:13:53 -0400

    Filled in linkedlist cycle analyzer class.

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -5,7 +5,34 @@ import support.CycleInfo;
 import support.LLNode;
 
 public class LinkedListCycleAnalyzer<T> {
-    public static <T> CycleInfo detectCycleInfo(LLNode<T> head){
-        throw new NotImplementedException("TODO: implement this method");
+    public static <T> CycleInfo detectCycleInfo(LLNode<T> head) {
+        LLNode<T> fast = head;
+        LLNode<T> slow = head;
+        int size = 1;
+        int index = 0;
+
+
+        while (fast != null && fast.getLink() != null) {
+            slow = slow.getLink();
+            fast = fast.getLink().getLink();
+
+            if (fast == slow) {
+                slow = head;
+
+                while (fast != slow) {
+                    slow = slow.getLink();
+                    fast = fast.getLink();
+                    index++;
+                }
+
+                LLNode<T> temp = slow.getLink();
+                while (slow != temp) {
+                    temp = temp.getLink();
+                    size++;
+                }
+                return new CycleInfo(index, size);
+            }
+        }
+        return new CycleInfo(-1, 0);
     }
 }
